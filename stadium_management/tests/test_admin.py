c76f64f909d95581d1215fc2fe2a971794694c61
mam <am.civil71@gmail.com>
2024-04-09 01:33:43 +0330

    test(stadium_management): team admin tests added

--- stadium_management/tests/test_admin.py
+++ stadium_management/tests/test_admin.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.urls import reverse
 
 from stadium_management.models import Stadium
+from stadium_management.models import Team
 
 
 class TestStadiumAdmin:
@@ -32,6 +33,36 @@ class TestStadiumAdmin:
         url = reverse(
             "admin:stadium_management_stadium_change",
             kwargs={"object_id": stadium.pk},
+        )
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
+
+
+class TestTeamAdmin:
+    def test_changelist(self, admin_client):
+        url = reverse("admin:stadium_management_team_changelist")
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
+
+    def test_add(self, admin_client):
+        url = reverse("admin:stadium_management_team_add")
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
+
+        response = admin_client.post(
+            url,
+            data={
+                "name": "name",
+            },
+        )
+        assert response.status_code == HTTPStatus.FOUND
+        assert Team.objects.filter(name="name").exists()
+
+    def test_view_stadium(self, admin_client):
+        team = Team.objects.create(name="name")
+        url = reverse(
+            "admin:stadium_management_team_change",
+            kwargs={"object_id": team.pk},
         )
         response = admin_client.get(url)
         assert response.status_code == HTTPStatus.OK
