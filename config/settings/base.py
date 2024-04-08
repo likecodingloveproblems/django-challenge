6ace87dbe67301223a8c1bd209b66f5b088fb763
mam <am.civil71@gmail.com>
2024-04-08 15:29:50 +0330

    feat: adding new app accounting

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,6 +94,7 @@ LOCAL_APPS = [
     "matchticketselling.users",
     # Your stuff: custom apps go here
     "stadium_management",
+    "accounting",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
