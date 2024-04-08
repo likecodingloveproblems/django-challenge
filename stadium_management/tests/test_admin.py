1102c1f9d88bec3f3b7a311819caa01e6926c4a3
mam <am.civil71@gmail.com>
2024-04-09 01:33:03 +0330

    test(stadium_management): stadium admin tests added

---
+++ stadium_management/tests/test_admin.py
@@ -0,0 +1,37 @@
+from http import HTTPStatus
+
+from django.urls import reverse
+
+from stadium_management.models import Stadium
+
+
+class TestStadiumAdmin:
+    def test_changelist(self, admin_client):
+        url = reverse("admin:stadium_management_stadium_changelist")
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
+
+    def test_add(self, admin_client):
+        url = reverse("admin:stadium_management_stadium_add")
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
+
+        response = admin_client.post(
+            url,
+            data={
+                "name": "name",
+                "description": "description",
+                "capacity": 1000,
+            },
+        )
+        assert response.status_code == HTTPStatus.FOUND
+        assert Stadium.objects.filter(name="name").exists()
+
+    def test_view_stadium(self, admin_client):
+        stadium = Stadium.objects.create(name="name", description="", capacity=1)
+        url = reverse(
+            "admin:stadium_management_stadium_change",
+            kwargs={"object_id": stadium.pk},
+        )
+        response = admin_client.get(url)
+        assert response.status_code == HTTPStatus.OK
